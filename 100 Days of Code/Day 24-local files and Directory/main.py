a519ff951bcbe365c53b5e4cd6ce3540c4be46d6
gopaldas11 <gopaldas535251@gmail.com>
2026-01-22 19:16:40 +0600

    Snake game is updated with higher scoew and local files and directories

---
+++ 100 Days of Code/Day 24-local files and Directory/main.py
@@ -0,0 +1,7 @@
+with open("../../my 24 day test.txt") as file:
+    contents = file.read()
+    print(contents)
+
+
+
+
--- 100 Days of Code/Snake game project Day 21 & 20/scoreboard.py
+++ 100 Days of Code/Snake game project Day 21 & 20/scoreboard.py
@@ -2,11 +2,12 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
-
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        with open("data.txt", mode="r") as data:
+            self.high_score = int(data.read())
         self.score = 0
         self.color("white")
         self.penup()
@@ -15,13 +16,17 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
